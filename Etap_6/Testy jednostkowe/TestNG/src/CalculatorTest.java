6971f9001a45f81f78a37e53ce899481ce163eb7
Zeazelu <50877478+Zeazelu@users.noreply.github.com>
2022-05-08 22:59:29 +0200

    Add files via upload

---
+++ Etap_6/Testy jednostkowe/TestNG/src/CalculatorTest.java
@@ -0,0 +1,48 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class CalculatorTest {
+    @Test
+    public void twoPlusTwoShouldEqualFour(){
+        var calculator = new Calculator();
+        assertTrue(calculator.add(2, 2) == 4);
+    }
+    @Test
+    public void twoMinusTwoShouldEqualZero(){
+        var calculator = new Calculator();
+        assertTrue(calculator.subtract(2, 2) == 0);
+    }
+    @Test
+    public void twoTimesTwoShouldEqualFour(){
+        var calculator = new Calculator();
+        assertTrue(calculator.multiply(2, 2) == 4);
+    }
+    @Test
+    public void twoDivideTwoShouldEqualOne(){
+        var calculator = new Calculator();
+        assertTrue(calculator.divide(2, 2) == 1);
+    }
+    /*
+    @Test
+    public void twoPlusTwoShouldEqualFour(){
+        var calculator = new Calculator();
+        assertEquals(4, calculator.add(2, 2));
+    }
+    @Test
+    public void twoMinusTwoShouldEqualZero(){
+        var calculator = new Calculator();
+        assertEquals(0, calculator.subtract(2, 2));
+    }
+    @Test
+    public void twoTimesTwoShouldEqualFour(){
+        var calculator = new Calculator();
+        assertEquals(4, calculator.multiply(2, 2));
+    }
+    @Test
+    public void twoDivideTwoShouldEqualOne(){
+        var calculator = new Calculator();
+        assertEquals(1, calculator.divide(2, 2));
+    }
+     */
+}
